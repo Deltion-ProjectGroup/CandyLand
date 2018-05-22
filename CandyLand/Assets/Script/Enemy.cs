181e8a0ba43c8ceee6fb636cec0aa31301142dd8
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2018-05-22 15:37:43 +0200

    Enemy NavMesh
    Can target the player

---
+++ CandyLand/Assets/Script/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : Character
+{
+    
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<NavMeshAgent>().SetDestination(GameObject.FindGameObjectWithTag("Player").transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
