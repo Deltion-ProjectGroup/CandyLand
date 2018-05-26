19b556ad19711acbda9c9cbdd3f9834873aa1aa5
PimHuisman <33859395+PimHuisman@users.noreply.github.com>
2018-05-26 23:23:54 +0200

    Add Enemy Behaviour
    Add Test Ground area
    add enemyrandom walker

---
+++ CandyLand/Assets/Script/Charaters/Enemies/Enemy.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : Character
+{
+    public GameObject target;
+    public RaycastHit searchRay;
+    NavMeshAgent agent;
+    public bool isChasing = false;
+
+    [Header("WalkArea")]
+    [SerializeField] float randomUnitCircleRadiusMin;
+    [SerializeField] float randomUnitCircleRadiusMax;
+    [SerializeField] float thinkTimerMin;
+    [SerializeField] float thinkTimerMax;
+    float thinkTimer;
+    public float attackTime = 1f;
+    public float attackTimeStart;
+    float randomUnitCircleRadius;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        thinkTimer -= Time.deltaTime;
+        {
+
+            if (attackTime <= 0)
+            {
+                Attack();
+                attackTime = attackTimeStart;
+            }
+
+            if (thinkTimer <= 0) // Subtract ThinkTimer over time.
+            {
+                randomUnitCircleRadius = Random.Range(randomUnitCircleRadiusMin, randomUnitCircleRadiusMax);
+                Think();
+                thinkTimer = Random.Range(thinkTimerMin, thinkTimerMax);
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            target = other.gameObject;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            target = null;
+        }
+    }
+    #region Normal Script
+    /*
+    public virtual void SetTarget(GameObject target)
+    {
+        gameObject.GetComponent<NavMeshAgent>().SetDestination(target.transform.position);//Sets the destination of the navMeshAgent to the sensed Player
+        //agent.SetDestination(target.transform.position);
+        Physics.Raycast(transform.position, transform.forward, out searchRay, 3);//Shoots out a raycast
+        if(searchRay.transform != null)//Checks if it at least hit an object with the raycast
+        {
+            if(searchRay.transform.gameObject.tag == "Player")//Checks if the hit object is a Player
+            {
+                Attack();// Plays the attack void
+            }
+        }
+    }
+    */
+    #endregion 
+
+
+    private void Think()
+    {
+        // Think if Not Chasing
+        if (isChasing == false)
+        {
+            // Pick a random point in the insideUnitCircle for X and Y and set it in a vector3
+            Vector3 newPos = transform.position + new Vector3(Random.insideUnitCircle.x * randomUnitCircleRadius, transform.position.y, Random.insideUnitCircle.y * randomUnitCircleRadius);
+            // Put the newPos in the setDestination
+            agent.SetDestination(newPos);
+        }
+        else if (health <= 0)
+        {
+            agent.enabled = false;
+        }
+    }
+
+    public virtual void Attack()
+    {
+        print("Attack Player");//Attacks THE FUCKING PLAYER
+    }
+}
---
+++ CandyLand/Assets/Script/Charaters/Player/Camerah.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerah : MonoBehaviour
+{
+    public enum MyEnum
+    {
+        wood,
+        stone,
+        iron,
+        done
+    }
+    private Vector3 rotatePos;
+    public float rotateMultiplier;
+    public float interactionRange;
+    public RaycastHit hit;
+    // Use this for initialization
+    void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rotateMultiplier = GetComponentInParent<Player>().rotateMultiplier;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RotateCam(rotatePos, rotateMultiplier);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FireRaycast(hit);
+        }
+        //Debug.DrawRay(transform.position, transform.forward, Color.cyan, 10);
+	}
+
+    public void RotateCam(Vector3 rotator, float speed)
+    {
+        rotator.x = - Input.GetAxis("Mouse Y");
+        transform.Rotate(rotator * speed * Time.deltaTime);
+    }
+
+    public void FireRaycast(RaycastHit hitter)
+    {
+        Physics.Raycast(transform.position, transform.forward, out hitter, interactionRange);
+        if(hitter.transform != null)
+        {
+            if(hitter.transform.gameObject.tag == "Interactable")
+            {
+                GetComponentInParent<Player>().Interact(hitter);
+            }
+        }
+    }
+}
