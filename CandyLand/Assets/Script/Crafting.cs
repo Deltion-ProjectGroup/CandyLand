38bf77cde046afcef6c76cb998dbbb44f3c257e2
Maurits Heetkamp <33858972+MauritsHeetkamp@users.noreply.github.com>
2018-05-26 02:20:29 +0200

    Gonna work on crafting
    Yep

---
+++ CandyLand/Assets/Script/Crafting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crafting : Interactable {
+    public GameObject craftingUI;
+    public GameObject[] craftingElements; //To find the components in the UI that will show the cost etc. later
+    public static Crafting crafting;
+	// Use this for initialization
+	void Start () {
+        crafting = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public override void Interact(GameObject interactor)
+    {
+        craftingUI.SetActive(true);
+    }
+}
---
+++ CandyLand/Assets/Script/CraftingItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CraftingItem : MonoBehaviour, IPointerEnterHandler
+{
+    public Itehm requiredItems;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = new Vector3(1.05f, 1.05f, 1.05f);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+    [System.Serializable]
+    public class Itehm
+    {
+        public PickUp requiredItem;
+        public int requiredAmt;
+    }
+}
