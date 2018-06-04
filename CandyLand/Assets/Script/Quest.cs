61eba16ac1c264099bc0f138346ab149fb230221
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2018-06-04 15:46:56 +0200

    Gonna work on quests
    Yeet

---
+++ CandyLand/Assets/Script/Quest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : MonoBehaviour {
+    public string questName;
+    public string npcName;
+    public string completionMsg;
+    public string progressMsg;
+    public string questDialog;
+    public bool completed;
+    public bool inProgress;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
