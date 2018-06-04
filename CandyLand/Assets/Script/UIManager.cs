fe4ca72a2659ed27dd3a8e920852475cdccc5539
Maurits Heetkamp <33858972+MauritsHeetkamp@users.noreply.github.com>
2018-06-04 22:24:32 +0200

    Added healthbar
    Refresh the health with the code
    UIManager.uiManager.RefreshHealth()
    
    It will make the healthbar change to its current health

---
+++ CandyLand/Assets/Script/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    public GameObject healthBar;
+    public UIManager uiManager;
+	// Use this for initialization
+	void Start () {
+        uiManager = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void RefreshHealth()
+    {
+        healthBar.GetComponent<Image>().fillAmount = (1 / GameObject.FindGameObjectWithTag("Player").GetComponent<Character>().maxHealth) * GameObject.FindGameObjectWithTag("Player").GetComponent<Character>().health;
+    }
+}
