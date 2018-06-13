7aac0e55dbd29211c1288291575d9eb5fe785b94
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2018-06-13 12:50:35 +0200

    NPC script added
    Forgot to quickly add it

---
+++ CandyLand/Assets/Script/NPC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : Interactable {
+    public List<string> dialogText = new List<string>();
+    public string charName;
+    public string charRole;
+    public bool hasStoryEffect;
+    public int storyEffectIndex;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public override void Interact(GameObject interactor)
+    {
+        UIManager.uiManager.Dialog(dialogText, charName, charRole, hasStoryEffect, storyEffectIndex);
+    }
+}
